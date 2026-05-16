c1b666495e8076246b1633dcdf68c23a2d97926e
SettTheArtist-LoveJ <desendiente273@gmail.com>
2026-05-16 16:02:37 -0400

    lol

--- src/react-app/LOVES/Love4.tsx
+++ src/react-app/LOVES/Love4.tsx
@@ -9,7 +9,7 @@ export default function Love4() {
     const ctx = canvas.getContext("2d")!;
 
     let particles: Particle[] = [];
-    const appearanceSpeed = 0.2;
+    const appearanceSpeed = 5;
 
     const offsetX = -55;
 
